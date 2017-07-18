115b91301902c28619646385ab2264e5100c8e70
Andrei Mungiu <andrei_mungiu@yahoo.com>
2017-07-18 20:30:26 +0200

    Files added via upload
    Still searching for a proper way to upload big projects on GitHub

---
+++ OdeToFoodDb.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Data.Entity;
+
+namespace OdeToFood.Models
+{
+    public class OdeToFoodDb : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<RestaurantReview> Reviews { get; set; }
+    }
+}
